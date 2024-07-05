14d2a2e379243c8b700c6be1854fda9b4acb6055
koushiro <koushiro.cqx@gmail.com>
2024-07-05 18:53:09 +0800

    update block_tag tracer attrs

--- src/middlewares/methods/block_tag.rs
+++ src/middlewares/methods/block_tag.rs
@@ -2,6 +2,7 @@ use std::sync::Arc;
 
 use async_trait::async_trait;
 use opentelemetry::trace::FutureExt;
+use opentelemetry::KeyValue;
 
 use crate::{
     extensions::api::EthApi,
@@ -113,11 +114,19 @@ impl Middleware<CallRequest, CallResult> for BlockTagMiddleware {
         context: TypeRegistry,
         next: NextFn<CallRequest, CallResult>,
     ) -> CallResult {
+        let request_methods = request.method.clone();
+        let request_params = serde_json::to_string(&request.params).expect("serialize JSON value shouldn't be fail");
         async move {
             let (request, context) = self.replace(request, context).await;
             next(request, context).await
         }
-        .with_context(TRACER.context("block_tag"))
+        .with_context(TRACER.context_with_attrs(
+            "block_tag",
+            [
+                KeyValue::new("method", request_methods),
+                KeyValue::new("params", request_params),
+            ],
+        ))
         .await
     }
 }
