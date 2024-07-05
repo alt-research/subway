44c666c54600cedcc5bc635659580476a48b69c7
koushiro <koushiro.cqx@gmail.com>
2024-07-05 19:26:13 +0800

    update client tracer attrs

--- src/extensions/client/mod.rs
+++ src/extensions/client/mod.rs
@@ -18,6 +18,7 @@ use jsonrpsee::{
     ws_client::{WsClient, WsClientBuilder},
 };
 use opentelemetry::trace::FutureExt;
+use opentelemetry::KeyValue;
 use rand::{seq::SliceRandom, thread_rng};
 use serde::Deserialize;
 use tokio::sync::Notify;
@@ -431,6 +432,8 @@ impl Client {
     }
 
     pub async fn request(&self, method: &str, params: Vec<JsonValue>) -> CallResult {
+        let request_params = serde_json::to_string(&params).expect("serialize JSON value shouldn't be fail");
+
         async move {
             let (tx, rx) = tokio::sync::oneshot::channel();
             self.sender
@@ -445,7 +448,7 @@ impl Client {
 
             rx.await.map_err(errors::internal_error)?.map_err(errors::map_error)
         }
-        .with_context(TRACER.context("client"))
+        .with_context(TRACER.context_with_attrs("client", [KeyValue::new("params", request_params)]))
         .await
     }
 
@@ -455,6 +458,8 @@ impl Client {
         params: Vec<JsonValue>,
         unsubscribe: &str,
     ) -> Result<Subscription<JsonValue>, Error> {
+        let subscribe_params = serde_json::to_string(&params).expect("serialize JSON value shouldn't be fail");
+
         async move {
             let (tx, rx) = tokio::sync::oneshot::channel();
             self.sender
@@ -470,7 +475,7 @@ impl Client {
 
             rx.await.map_err(errors::internal_error)?
         }
-        .with_context(TRACER.context("client"))
+        .with_context(TRACER.context_with_attrs("client", [KeyValue::new("params", subscribe_params)]))
         .await
     }
 
